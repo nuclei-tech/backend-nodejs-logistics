972ab18ef64b10a6995b2458cb2b8c7c1129e9ca
Alexander Graebe <alex.graebe@gmail.com>
2020-01-10 14:00:49 -0800

    Add logging, fix sample

--- common/logistics/logistics.js
+++ common/logistics/logistics.js
@@ -11,6 +11,7 @@ function initLogisticsSamples() {
   // set up all sample drivers
   // note: ensure the sample JSON file has drivers with the correct device_id
   for (let i = 0; i < sampleDrivers.length; i++) {
+    console.log(`Add sample driver "${sampleDrivers[i].driver_id}"...`);
     request({
       url: `${API_URL}/drivers`,
       method: "POST",
@@ -20,6 +21,7 @@ function initLogisticsSamples() {
 
   // set up all sample deliveries
   for (let i = 0; i < sampleDeliveries.length; i++) {
+    console.log(`Add sample delivery "${sampleDeliveries[i].delivery_id}"...`);
     request({
       url: `${API_URL}/deliveries`,
       method: "POST",
