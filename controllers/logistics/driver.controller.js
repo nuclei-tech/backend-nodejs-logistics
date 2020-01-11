84f48e0bedbdf2526aff61c61f60fb1f1cb3ab98
Alexander Graebe <alex.graebe@gmail.com>
2020-01-10 19:11:58 -0800

    Adding geocoder for deliveries

--- controllers/logistics/driver.controller.js
+++ controllers/logistics/driver.controller.js
@@ -1,3 +1,5 @@
+var NodeGeocoder = require("node-geocoder");
+
 const Driver = require("../../models/logistics/driver.model");
 const Delivery = require("../../models/logistics/delivery.model");
 
@@ -68,7 +70,75 @@ exports.findOne = (req, res) => {
 
 // TODO: Find a single driver with a driver_id and check in
 exports.findOneAndCheckin = (req, res) => {
-  res.status(500).send("Not implemented");
+  const { body } = req;
+  // verify token
+  if (!body.token) {
+    res.status(400).send("Error: A token is required to checkin");
+  }
+
+  Driver.findOne({ driver_id: req.params.driver_id })
+    .then(async driver => {
+      if (!driver) {
+        return res.status(404).send({
+          message: "Driver not found with id " + req.params.driver_id
+        });
+      }
+
+      // verify device_id
+      if (!driver.device_id) {
+        res
+          .status(400)
+          .send("Error: A driver device_id is required to checkin");
+      }
+
+      // Add deliveries to driver
+      const resp = await Delivery.find({
+        driver_id: driver.driver_id
+      });
+      if (resp) {
+        driver.deliveries = resp;
+
+        // convert address to lat/lng for each trip
+        const geocoder = NodeGeocoder({
+          provider: "google",
+          apiKey: process.env.GMAPS_KEY,
+          formatter: null
+        });
+
+        for (let i = 0; i < resp.length; i++) {
+          const delivery = resp[i];
+
+          const geoResp = await geocoder.geocode({
+            address: delivery.address.street,
+            country: delivery.address.country,
+            zipcode: delivery.address.postalCode
+          });
+
+          if (geoResp.length > 0) {
+            delivery.lat = geoResp[0].latitude;
+            delivery.lng = geoResp[0].longitude;
+          }
+        }
+
+        console.log(resp);
+        // create trip
+      }
+
+      // update active_trip and token
+      driver.token = body.token;
+
+      res.send(driver);
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).send({
+          message: "Driver not found with id " + req.params.driver_id
+        });
+      }
+      return res.status(500).send({
+        message: "Error retrieving driver with id " + req.params.driver_id
+      });
+    });
 };
 
 // Update a driver using driver_id
