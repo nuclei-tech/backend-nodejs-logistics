875855b5e244afa542fa74e448072bc0c2984bd4
Alexander Graebe <alex.graebe@gmail.com>
2020-02-18 15:03:49 -0800

    Remove base URL

--- common/logistics/logistics.js
+++ common/logistics/logistics.js
@@ -4,16 +4,13 @@ const _ = require("lodash");
 const sampleDrivers = require("./drivers.json");
 const sampleDeliveries = require("./deliveries.json");
 
-const API_URL = `${process.env.BASE_URL || "http://localhost"}:${process.env
-  .PORT || 8080}`;
-
 function initLogisticsSamples() {
   // set up all sample drivers
   // note: ensure the sample JSON file has drivers with the correct device_id
   for (let i = 0; i < sampleDrivers.length; i++) {
     console.log(`Add sample driver "${sampleDrivers[i].driver_id}"...`);
     request({
-      url: `${API_URL}/logistics/drivers`,
+      url: "/logistics/drivers",
       method: "POST",
       json: sampleDrivers[i]
     });
@@ -23,7 +20,7 @@ function initLogisticsSamples() {
   for (let i = 0; i < sampleDeliveries.length; i++) {
     console.log(`Add sample delivery "${sampleDeliveries[i].delivery_id}"...`);
     request({
-      url: `${API_URL}/logistics/deliveries`,
+      url: "/logistics/deliveries",
       method: "POST",
       json: sampleDeliveries[i]
     });
